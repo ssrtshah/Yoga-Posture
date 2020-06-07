2c31a4536d90ad803332c31a3fac92a7b2e74396
ssrtshah <64350809+ssrtshah@users.noreply.github.com>
2020-06-07 12:32:26 +0530

    edited

--- OpenPose/runtwoimages.py
+++ OpenPose/runtwoimages.py
@@ -8,7 +8,7 @@ diff=[]
 capture.captureImage()
 
 opi.OpenImage(o1,"new_image.jpeg","Output-Keypoints.jpg","Output-Skeleton.jpg")
-opi.OpenImage(o2,"v11.jpg","Output-Keypoints1.jpg","Output-Skeleton1.jpg")
+opi.OpenImage(o2,"v1.jpg","Output-Keypoints1.jpg","Output-Skeleton1.jpg")
 
 for i in range(len(o1)):
 	if o1[i]!=0 and o2[i]!=0:

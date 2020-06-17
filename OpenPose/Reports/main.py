02e5451bf0b453c7a3b41824bf02180e39a3fd76
PriyaC20 <54627145+PriyaC20@users.noreply.github.com>
2020-06-17 15:26:17 +0530

    Update main.py

--- OpenPose/Reports/main.py
+++ OpenPose/Reports/main.py
@@ -3,6 +3,9 @@ import db
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('main.html')
 
 @app.route('/UpdateWeight')
 def changeweight():
@@ -14,7 +17,16 @@ def changeweight():
 @app.route('/userReport')
 def generateReport():
     userID = int(request.args.get('userid')) 
+    print( db.userReport(userID))
     return db.userReport(userID)
+
+@app.route('/prevUserReport')
+def generatePrevReport():
+    userID = int(request.args.get('userid')) 
+    month = int(request.args.get('month'))
+    print( db.prevUserReport(userID , month))
+    return db.prevUserReport(userID , month)
+
 
 @app.route('/updateActivity')
 def updateActivity():
@@ -28,7 +40,13 @@ def newUser():
     userID = int(request.args.get('userid')) 
     weight = int(request.args.get('weight'))
     db.newUser(userID,weight)
-    return ' ' 
+    return ' '
+    
+@app.route('/resetProgress')
+def reset():
+    userID = int(request.args.get('userid')) 
+    db.resetReport(userID)
+    return ' '
 
 if __name__ == "__main__":
     app.run(debug = True)

dbb8b3704c51e8646883b171a88d1c7525f847b0
ssrtshah <64350809+ssrtshah@users.noreply.github.com>
2020-06-22 21:19:07 +0530

    added files

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import flask as f 
 import runmain as rm
 import firebase
+import Reports.db as db
 
 
 config = {
@@ -41,6 +42,45 @@ def geterror():
 	errormsg=rm.findingerror()
 	d['msg']=errormsg
 	return f.jsonify(d)
+
+@app.route('/UpdateWeight')
+def changeweight():
+    userID = f.request.args.get('userid')
+    newWeight = int(f.request.args.get('weight'))
+    db.updateWeight(userID , newWeight)
+    return 'Weight updated successfully'
+
+@app.route('/userReport')
+def generateReport():
+    userID = f.request.args.get('userid')
+    return f.jsonify(db.userReport(userID))
+
+@app.route('/prevUserReport')
+def generatePrevReport():
+    userID = f.request.args.get('userid')
+    month = int(f.request.args.get('month'))
+    return f.jsonify(db.prevUserReport(userID , month))
+
+
+@app.route('/updateActivity')
+def updateActivity():
+    userID = f.request.args.get('userid') 
+    minutes = int(f.request.args.get('minutes'))
+    db.updateUserActivity(userID,minutes)
+    return 'Activity Recorded'
+
+@app.route('/createUser')
+def newUser():
+    userID = f.request.args.get('userid')
+    weight = int(f.request.args.get('weight'))
+    db.newUser(userID,weight)
+    return ' '
+    
+@app.route('/resetProgress')
+def reset():
+    userID = f.request.args.get('userid')
+    db.resetReport(userID)
+    return ' '
 
 
 if __name__ == '__main__':
